a050ff604253d0b3d9a520531291ebeb64f6337e
icarus vagner <ruthakaza@gmail.com>
2025-09-20 23:05:18 +0800

    initial commit

--- src/gui/components/header.rs
+++ src/gui/components/header.rs
@@ -1,17 +1,19 @@
 use iced::{
-    alignment::{Horizontal, Vertical},
-    widget::{button, container, horizontal_space, text, Row},
-    Element, Length,
+	alignment::{Horizontal, Vertical},
+	widget::{button, container, horizontal_space, text, Row},
+	Element, Length,
 };
 
 use crate::{
-    gui::{
-        morphiq::Morphiq,
-        pages::home::{ContentView, HomeMessage, OpenSettings},
-        styles::{button::ButtonType, container::ContainerType, types::style_type::StyleType},
-        types::message::Message,
-    },
-    utils::types::icon::Icon,
+	gui::{
+		morphiq::Morphiq,
+		pages::home::{ContentView, HomeMessage, OpenSettings},
+		styles::{
+			button::ButtonType, container::ContainerType, types::style_type::StyleType,
+		},
+		types::message::Message,
+	},
+	utils::types::icon::Icon,
 };
 
 #[derive(Debug, Default, Clone)]
@@ -21,147 +23,143 @@ pub struct Header;
 #[allow(clippy::large_enum_variant)]
 #[derive(Debug, Clone)]
 pub enum HeaderMessage {
-    Dashboard,
-    Search,
-    AddEmployee,
-    EventPosting,
-    OrgSettings,
-    Language,
-    Chat,
-    Notifications,
-    Profile,
+	Dashboard,
+	Search,
+	AddEmployee,
+	EventPosting,
+	OrgSettings,
+	Language,
+	Chat,
+	Notifications,
+	Profile,
 }
 
 impl Header {
-    fn header_btn<'a>(
-        &'a self,
-        title: Option<String>,
-        icon: Icon,
-        message: Message,
-    ) -> Element<'a, Message, StyleType> {
-        if let Some(title) = title {
-            let content = Row::new()
-                .push(
-                    icon.to_text()
-                        .size(18.0)
-                        .align_x(Horizontal::Center)
-                        .align_y(Vertical::Center),
-                )
-                .push(
-                    text(title)
-                        .size(18.0)
-                        .align_x(Horizontal::Center)
-                        .align_y(Vertical::Center)
-                        .line_height(text::LineHeight::Relative(1.7)),
-                )
-                .spacing(5.0)
-                .align_y(Vertical::Center);
-            button(content)
-                .on_press(message)
-                .class(ButtonType::Base100)
-                .into()
-        } else {
-            button(
-                icon.to_text()
-                    .size(22)
-                    .align_x(Horizontal::Center)
-                    .align_y(Vertical::Center),
-            )
-            .class(ButtonType::Base100)
-            .on_press(message)
-            .into()
-        }
-    }
+	fn header_btn<'a>(
+		&'a self,
+		title: Option<String>,
+		icon: Icon,
+		message: Message,
+	) -> Element<'a, Message, StyleType> {
+		if let Some(title) = title {
+			let content = Row::new()
+				.push(icon
+					.to_text()
+					.size(18.0)
+					.align_x(Horizontal::Center)
+					.align_y(Vertical::Center))
+				.push(text(title)
+					.size(18.0)
+					.align_x(Horizontal::Center)
+					.align_y(Vertical::Center)
+					.line_height(text::LineHeight::Relative(1.7)))
+				.spacing(5.0)
+				.align_y(Vertical::Center);
+			button(content)
+				.on_press(message)
+				.class(ButtonType::Base100)
+				.into()
+		} else {
+			button(icon
+				.to_text()
+				.size(22)
+				.align_x(Horizontal::Center)
+				.align_y(Vertical::Center))
+			.class(ButtonType::Base100)
+			.on_press(message)
+			.into()
+		}
+	}
 
-    fn left_menu<'a>(&'a self) -> Element<'a, Message, StyleType> {
-        Row::new()
-            .push(self.header_btn(
-                None,
-                Icon::Morphiq,
-                Message::Home(HomeMessage::Content(ContentView::Dashboard)),
-            ))
-            .push(self.header_btn(
-                Some("Search".to_string()),
-                Icon::Search,
-                Message::Home(HomeMessage::Content(ContentView::Search)),
-            ))
-            .push(self.header_btn(
-                Some("Add Employee".to_string()),
-                Icon::UserRoundPlus,
-                Message::Home(HomeMessage::Content(ContentView::AddEmployee)),
-            ))
-            .push(self.header_btn(
-                Some("Event Postings".to_string()),
-                Icon::CalendarDays,
-                Message::Home(HomeMessage::Content(ContentView::EventsPosting)),
-            ))
-            .push(self.header_btn(
-                Some("Organization Settings".to_string()),
-                Icon::LayoutPanelLeft,
-                Message::Home(HomeMessage::Content(ContentView::Settings(
-                    OpenSettings::OrgSettings,
-                ))),
-            ))
-            .spacing(5)
-            .align_y(Vertical::Center)
-            .into()
-    }
+	fn left_menu<'a>(&'a self) -> Element<'a, Message, StyleType> {
+		Row::new()
+			.push(self.header_btn(
+				None,
+				Icon::Morphiq,
+				Message::Home(HomeMessage::Content(ContentView::Dashboard)),
+			))
+			.push(self.header_btn(
+				Some("Search".to_string()),
+				Icon::Search,
+				Message::Home(HomeMessage::Content(ContentView::Search)),
+			))
+			.push(self.header_btn(
+				Some("Add Employee".to_string()),
+				Icon::UserRoundPlus,
+				Message::Home(HomeMessage::Content(ContentView::AddEmployee)),
+			))
+			.push(self.header_btn(
+				Some("Event Postings".to_string()),
+				Icon::CalendarDays,
+				Message::Home(HomeMessage::Content(ContentView::EventsPosting)),
+			))
+			.push(self.header_btn(
+				Some("Organization Settings".to_string()),
+				Icon::LayoutPanelLeft,
+				Message::Home(HomeMessage::Content(ContentView::Settings(
+					OpenSettings::OrgSettings,
+				))),
+			))
+			.spacing(5)
+			.align_y(Vertical::Center)
+			.into()
+	}
 
-    fn right_menu<'a>(&'a self, morphiq: &Morphiq) -> Element<'a, Message, StyleType> {
-        let toggle_theme = if morphiq.configs.settings.style == StyleType::Dark {
-            Icon::Sun
-        } else {
-            Icon::Moon
-        };
+	fn right_menu<'a>(&'a self, morphiq: &Morphiq) -> Element<'a, Message, StyleType> {
+		let toggle_theme = if morphiq.configs.settings.style == StyleType::Dark {
+			Icon::Sun
+		} else {
+			Icon::Moon
+		};
 
-        Row::new()
-            .push(self.header_btn(None, toggle_theme, Message::ChangeTheme))
-            .push(self.header_btn(
-                None,
-                Icon::Globe,
-                Message::Home(HomeMessage::Content(ContentView::Settings(
-                    OpenSettings::Languages,
-                ))),
-            ))
-            .push(self.header_btn(
-                None,
-                Icon::BellRing,
-                Message::Home(HomeMessage::Content(ContentView::Notifications)),
-            ))
-            .push(self.header_btn(
-                None,
-                Icon::UserRound,
-                Message::Home(HomeMessage::Content(ContentView::Profile)),
-            ))
-            .spacing(5)
-            .align_y(Vertical::Center)
-            .into()
-    }
+		Row::new()
+			.push(self.header_btn(None, toggle_theme, Message::ChangeTheme))
+			.push(self.header_btn(
+				None,
+				Icon::Globe,
+				Message::Home(HomeMessage::Content(ContentView::Settings(
+					OpenSettings::Languages,
+				))),
+			))
+			.push(self.header_btn(
+				None,
+				Icon::BellRing,
+				Message::Home(HomeMessage::Content(ContentView::Notifications)),
+			))
+			.push(self.header_btn(
+				None,
+				Icon::UserRound,
+				Message::Home(HomeMessage::Content(ContentView::Profile)),
+			))
+			.spacing(5)
+			.align_y(Vertical::Center)
+			.into()
+	}
 
-    pub(crate) fn update(&mut self, message: HeaderMessage) {
-        match message {
-            HeaderMessage::Dashboard => {}
-            HeaderMessage::Search => {}
-            HeaderMessage::AddEmployee => {}
-            HeaderMessage::EventPosting => {}
-            HeaderMessage::OrgSettings => {}
-            HeaderMessage::Language => {}
-            HeaderMessage::Chat => {}
-            HeaderMessage::Notifications => {}
-            HeaderMessage::Profile => {}
-        }
-    }
+	pub(crate) fn update(&mut self, message: HeaderMessage) {
+		match message {
+			HeaderMessage::Dashboard => {}
+			HeaderMessage::Search => {}
+			HeaderMessage::AddEmployee => {}
+			HeaderMessage::EventPosting => {}
+			HeaderMessage::OrgSettings => {}
+			HeaderMessage::Language => {}
+			HeaderMessage::Chat => {}
+			HeaderMessage::Notifications => {}
+			HeaderMessage::Profile => {}
+		}
+	}
 
-    pub(crate) fn view<'a>(&'a self, morphiq: &Morphiq) -> Element<'a, Message, StyleType> {
-        let content = Row::new()
-            .push(self.left_menu())
-            .push(horizontal_space())
-            .push(self.right_menu(morphiq))
-            .align_y(Vertical::Center);
+	pub(crate) fn view<'a>(&'a self, morphiq: &Morphiq) -> Element<'a, Message, StyleType> {
+		let content = Row::new()
+			.push(self.left_menu())
+			.push(horizontal_space())
+			.push(self.right_menu(morphiq))
+			.align_y(Vertical::Center);
 
-        container(content)
-            .width(Length::Fill)
-            .class(ContainerType::Ghost)
-            .into()
-    }
+		container(content)
+			.width(Length::Fill)
+			.class(ContainerType::Ghost)
+			.into()
+	}
 }
--- src/gui/types/message.rs
+++ src/gui/types/message.rs
@@ -1,34 +1,34 @@
 use iced::window;
 
 use crate::gui::{
-    pages::{home::HomeMessage, login::LoginMessage},
-    styles::types::style_type::StyleType,
+	pages::{home::HomeMessage, login::LoginMessage},
+	styles::types::style_type::StyleType,
 };
 
 #[allow(clippy::large_enum_variant)]
 #[derive(Debug, Clone)]
 pub enum Message {
-    /// Run task to initialize app
-    StartApp(Option<window::Id>),
-    /// Sent by the backend to get the data to display
-    TickDashboard,
-    /// Change application Style
-    Style(StyleType),
-    /// Hides the current settings view
-    CloseSettings,
-    /// Set notification volume
-    ChangeVolume(u8),
-    /// Change the appearance of the system
-    ChangeTheme,
-    /// Save the configuration of the app and quit
-    Quit,
-    /// Start with some dashboard data
-    Start,
-    /// Fetch mock chat data
-    FetchChats,
+	/// Run task to initialize app
+	StartApp(Option<window::Id>),
+	/// Sent by the backend to get the data to display
+	TickDashboard,
+	/// Change application Style
+	Style(StyleType),
+	/// Hides the current settings view
+	CloseSettings,
+	/// Set notification volume
+	ChangeVolume(u8),
+	/// Change the appearance of the system
+	ChangeTheme,
+	/// Save the configuration of the app and quit
+	Quit,
+	/// Start with some dashboard data
+	Start,
+	/// Fetch mock chat data
+	FetchChats,
 
-    // Messages for views
-    /// Login view message
-    Login(LoginMessage),
-    Home(HomeMessage),
+	// Messages for views
+	/// Login view message
+	Login(LoginMessage),
+	Home(HomeMessage),
 }
