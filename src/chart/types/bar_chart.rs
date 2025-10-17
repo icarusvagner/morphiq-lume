e59e0c844351efda556f8d3aa62f6f7ef8a747e3
icarusvagner <icarusvagner@gmail.com>
2025-10-17 15:08:49 +0800

    initial commit

--- src/chart/types/bar_chart.rs
+++ src/chart/types/bar_chart.rs
@@ -1,11 +1,38 @@
-use iced::Font;
+use std::f64;
+
+use iced::{
+	Color,
+	Font,
+	Renderer,
+	alignment::{
+		Horizontal,
+		Vertical,
+	},
+	widget::canvas::{
+		self,
+		Frame,
+		Path,
+		Text,
+	},
+};
+
+use crate::gui::{
+	styles::{
+		bar::Catalog,
+		style_constant::{
+			CHART_BAR_WIDTH,
+			fonts::FONT_SIZE_TITLE,
+		},
+	},
+	types::message::Message,
+};
 
 #[derive(Clone, Debug)]
 pub struct BarChart {
-	pub title: String,
-	pub labels: Vec<String>,
-	pub values: Vec<f64>,
-	pub font: Font,
+	title: String,
+	labels: Vec<String>,
+	values: Vec<f64>,
+	font: Font,
 }
 
 impl BarChart {
@@ -20,5 +47,34 @@ impl BarChart {
 
 	fn title(&self) -> String {
 		self.title.clone()
+	}
+
+	fn max_value(&self) -> f64 {
+		self.values.iter().copied().fold(f64::NEG_INFINITY, f64::max)
+	}
+}
+
+impl<Message, Theme: Catalog> canvas::Program<Message, Theme> for BarChart {
+	type State = ();
+
+	fn draw(
+		&self,
+		state: &Self::State,
+		renderer: &Renderer,
+		theme: &Theme,
+		bounds: iced::Rectangle,
+		cursor: iced::advanced::mouse::Cursor,
+	) -> Vec<canvas::Geometry> {
+		let mut frame = Frame::new(renderer, bounds.size());
+		let style =
+			<Theme as Catalog>::style(theme, &<Theme as Catalog>::default());
+		let colors = [style.primary, style.secondary, style.accent];
+
+		let total_width = bounds.width;
+		let mut y_offset: f64 = 0.0;
+		let left_padding = 13.0;
+		let usable_width = bounds.width - left_padding;
+
+		vec![frame.into_geometry()]
 	}
 }
--- src/gui/styles/style_constant.rs
+++ src/gui/styles/style_constant.rs
@@ -7,6 +7,7 @@ pub const CHARTS_LINE_BORDER: u32 = 1;
 pub const BORDER_ROUNDED_RADIUS: f32 = 8.0;
 pub const BORDER_RADIUS: f32 = 8.0;
 pub const BORDER_BUTTON_RADIUS: f32 = 180.0;
+pub const CHART_BAR_WIDTH: f64 = 24.0;
 
 #[allow(clippy::enum_variant_names, clippy::large_enum_variant)]
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
